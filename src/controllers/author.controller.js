8546a097f9c857b7f69f4a4d5e92ad1edc1d2ecd
SantiRendon <srendon34@soy.sena.edu.co>
2023-04-06 00:49:49 -0500

    ADDED author.controller and routes in app.js

--- src/controllers/author.controller.js
+++ src/controllers/author.controller.js
@@ -1,17 +1,17 @@
 import { getConnection } from "../db/database"
 // interacciones con la base de datos
 
-//! GET
+//TODO: Solucionar error, el get por id funciona pero el general no
 const getAuthors = async (req, res) => { // GET ALL
     try {
         const connection = await getConnection();
-        const result = await connection.query(`CALL spGetAuthor()`); // GET = SELECT
+        const result = await connection.query(`CALL spGetAllAuthor();`); // GET = SELECT
         console.log(result);
 
         res.json(result);
     } catch (error) {
         res.status(500);
-        res.send(error.message);
+        res.send(error);
     }
 };
 const getAuthor = async (req, res) => { // Get for ID
@@ -20,28 +20,28 @@ const getAuthor = async (req, res) => { // Get for ID
         const { id } = req.params;
 
         const connection = await getConnection();
-        const result = await connection.query(`CALL spGetAuthor(${id})`); // GET = SELECT
+        const result = await connection.query(`CALL spGetAuthorForId(${id})`); // GET = SELECT
 
-        res.json(result);
+        res.json(result[0]);
     } catch (error) {
         res.status(500);
         res.send(error.message);
     }
 };
 
-//! POST
+//* POST
 const addAuthor = async (req, res) => {
     try {
-        const { row1,row2 } = req.body;
+        const { NOM_AUTOR, FECHA_NACIMIENTO, LUGAR_NACIMIENTO, FECHA_MUERTE, OCUPACIONES, MOVIMIENTO_LITERARIO } = req.body;
 
-        if (row1 === undefined) {
-           return res.status(400).json({ message: "Bad request. Please fill all field." })
+        if (NOM_AUTOR === undefined || FECHA_NACIMIENTO === undefined || LUGAR_NACIMIENTO === undefined || FECHA_MUERTE === undefined || OCUPACIONES === undefined || MOVIMIENTO_LITERARIO === undefined) {
+            return res.status(400).json({ message: "Bad request. Please fill all field." })
         }
 
-        const Author = { row1,row2 };
+        const Author = { NOM_AUTOR, FECHA_NACIMIENTO, LUGAR_NACIMIENTO, FECHA_MUERTE, OCUPACIONES, MOVIMIENTO_LITERARIO };
         const connection = await getConnection();
 
-        const result = await connection.query(`CALL spAddAuthor(${Author})`);
+        const result = await connection.query(`CALL spInsertAuthor('${Author.NOM_AUTOR}', '${Author.FECHA_NACIMIENTO}', '${Author.LUGAR_NACIMIENTO}', '${Author.FECHA_MUERTE}', '${Author.OCUPACIONES}', '${Author.MOVIMIENTO_LITERARIO}')`);
 
         // res.json(result); //* Ver informacion completa de la consulta
         res.json({ message: "Author Added" });
@@ -52,7 +52,7 @@ const addAuthor = async (req, res) => {
     }
 };
 
-//! DELETE
+//* DELETE
 const deleteAuthor = async (req, res) => {
     try {
         console.log(req.params);
@@ -68,19 +68,20 @@ const deleteAuthor = async (req, res) => {
     }
 };
 
-//! PUT
+//* PUT
 const updateAuthor = async (req, res) => {
     try {
         const { id } = req.params;
-        const { row1,row2 } = req.body;
-        const Author = { row1,row2 }
-
-        if (id === undefined || row1 === undefined) {
-           return res.status(400).json({ message: "Bad request. Please fill all field." })
+        const { NOM_AUTOR, FECHA_NACIMIENTO, LUGAR_NACIMIENTO, FECHA_MUERTE, OCUPACIONES, MOVIMIENTO_LITERARIO } = req.body;
+        
+        if (id === undefined || NOM_AUTOR === undefined || FECHA_NACIMIENTO === undefined || LUGAR_NACIMIENTO === undefined || FECHA_MUERTE === undefined || OCUPACIONES === undefined || MOVIMIENTO_LITERARIO === undefined) {
+            return res.status(400).json({ message: "Bad request. Please fill all field." })
         }
+        
+        const Author = { NOM_AUTOR, FECHA_NACIMIENTO, LUGAR_NACIMIENTO, FECHA_MUERTE, OCUPACIONES, MOVIMIENTO_LITERARIO };
 
         const connection = await getConnection();
-        const result = await connection.query(`CALL spUpdateAuthor(${id},'${Author}')`);
+        const result = await connection.query(`CALL spUpdateAuthor(${id}, '${Author.NOM_AUTOR}', '${Author.FECHA_NACIMIENTO}', '${Author.LUGAR_NACIMIENTO}', '${Author.FECHA_MUERTE}', '${Author.OCUPACIONES}', '${Author.MOVIMIENTO_LITERARIO}')`);
 
         res.json(result);
     } catch (error) {
