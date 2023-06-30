4dbf5ab810a4f0e5cf04cd44a5402480414d7133
SantiRendon <srendon34@soy.sena.edu.co>
2023-06-30 00:31:11 -0500

    actualizando api para subir a produccion 2

--- src/config/enviroment/index.js
+++
@@ -1,8 +0,0 @@
-import path from "path";
-import dontenv, { config } from "dotenv";
-
-dontenv.config({
-    "path":path.resolve(__dirname,`${process.env.NODE_ENV}.env`)
-});
-
-export default config;
--- src/index.js
+++
@@ -1,9 +0,0 @@
-// funcion principal
-import app from "./app";
-import { caseEntorno } from "./config";
-
-const main=()=>{
-    app.listen(app.get("PORT"),caseEntorno);
-};
-
-main();
